5446566c963e289d54837bd41157df19021a479f
Erick Canales <e94canales@gmail.com>
2020-04-18 15:52:05 -0700

    added api and functional currenttime pos

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import ProgressBarContainer from '../components/styles/ProgressBar'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons'
